6fd4feda1b83e39210bbba640c0012cb0fc33021
skynocover <skynocover@Gmail.com>
2021-12-06 14:18:06 +0800

    update

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api/version', (req, res) => {
+  res.json({ version: '1.0.0' });
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(new Date(), `server listening on ${port}`);
+});
